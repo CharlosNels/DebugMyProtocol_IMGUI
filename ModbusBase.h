d8676f3fdca1bb339769ffc8969fff19cdcc6473
taki <netassasinc@outlook.com>
2024-08-19 21:20:24 +0800

    添加中文支持

--- ModbusBase.h
+++ ModbusBase.h
@@ -7,6 +7,7 @@ class ModbusBase
 {
 public:
     ModbusBase(){};
+    virtual ~ModbusBase(){};
     virtual bool validPack(const char *buffer, size_t buffer_size) = 0;
     virtual ModbusFrameInfo masterPack2Frame(const char *buffer, size_t buffer_size) = 0;
     virtual size_t masterFrame2Pack(const ModbusFrameInfo &frame_info, char *buffer) = 0;
